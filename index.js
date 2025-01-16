d951286831d0af2917720629c61e18e4c553e7b4
Bangdx1412 <email_lin_kt_vi_Bangdx1412>
2025-01-16 15:05:09 +0700

    Nhúng file tĩnh public

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const route = require("./routes/client/index.route");
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.static("public")); // Nhúng giao diện tĩnh
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
